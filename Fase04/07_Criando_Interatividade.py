1e9039a17b64cdef32af44b015e71c679034fd0e
Leonardo Oliveira <leonardoc.developer@gmail.com>
2020-04-15 00:33:05 -0400

    Interatividade com o usuário, conhecendo o input()

---
+++ Fase04/07_Criando_Interatividade.py
@@ -0,0 +1,9 @@
+# Fase 04 - Primeiro Comando em Python
+# Teoria
+# Conhecendo a função input()
+
+nome = input('Qual é o seu nome ? ')
+idade = input('Quantos anos você tem ? ')
+peso = input('Qual é o seu peso ? ')
+print(nome, idade, peso)
+
