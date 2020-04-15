b8f32b196cb6f3d7d30278ca9a19ed28c9ba2b81
Leonardo Oliveira <leonardoc.developer@gmail.com>
2020-04-15 08:45:06 -0400

    Desafio 01 - Mensagem de Boas-vindas de acordo com o valor digitado

---
+++ Fase04/Desafios/Desafio01.py
@@ -0,0 +1,9 @@
+#   Fase 04 - Primeiro Comando em Python
+#   Desafio 01
+#   Crie um script Python que leia o nome de uma pessoa
+#   e mostre uma mensagem de boas-vindas de acordo com o valor digitado.
+
+nome = input('Qual é o seu nome ? ')
+
+print('Seja Bem-vindo', nome, '!')
+
