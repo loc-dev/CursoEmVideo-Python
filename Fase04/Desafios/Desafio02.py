7b024fd2f95f59a8e791c80e7deb18e5f29eb650
Leonardo Oliveira <leonardoc.developer@gmail.com>
2020-04-15 23:25:38 -0400

    Desafio 02 - Mensagem com a data de nascimento formatada

---
+++ Fase04/Desafios/Desafio02.py
@@ -0,0 +1,13 @@
+#   Fase 04 - Primeiro Comando em Python
+#   Desafio 02
+#   Crie um script Python que leia o dia, o mês e o ano de nascimento de uma pessoa
+#   e mostre uma mensagem com a data formatada.
+
+nome = input('Digite o seu nome: ')
+dia = input('Digite o dia: ')
+mes = input('Digite o mês: ')
+ano = input('Digite o ano: ')
+
+print('Olá,', nome)
+print('Sua data de nascimento é, ', dia, 'de', mes, 'de', ano)
+
