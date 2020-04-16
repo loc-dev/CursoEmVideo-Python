b0392305e26b25d3cb603e673ae91cd9f5a5c67a
Leonardo Oliveira <leonardoc.developer@gmail.com>
2020-04-16 00:15:45 -0400

    Desafio 03 - Mostrar a soma entre dois valores digitados

---
+++ Fase04/Desafios/Desafio03.py
@@ -0,0 +1,12 @@
+#   Fase 04 - Primeiro Comando em Python
+#   Desafio 03
+#   Crie um script Python que leia dois números e tente mostrar a soma entre eles.
+
+n_01 = int(input('Digite o primeiro valor: '))
+n_02 = int(input('Digite o segundo valor: '))
+
+s = (n_01 + n_02)
+
+print('A sua operação é:')
+print(n_01, '+', n_02, '=', s)
+
