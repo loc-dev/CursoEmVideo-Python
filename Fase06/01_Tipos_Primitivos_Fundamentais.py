f701469b1234be838493ba22b6f2c749d5b1fe17
Leonardo Oliveira <leonardoc.developer@gmail.com>
2020-04-18 23:51:37 -0400

    Apresentando os quatros Tipos Primitivos fundamentais na linguagem de programação

---
+++ Fase06/01_Tipos_Primitivos_Fundamentais.py
@@ -0,0 +1,19 @@
+# Fase 06 - Tipos Primitivos e Saída de Dados
+# Teoria
+# Os quatro tipos primitivos fundamentais
+
+# Tipo Primitivo String (str)
+valor_01 = input('Digite qualquer coisa? ')
+print(type(valor_01))
+
+# Tipo Primitivo Integer (int)
+valor_02 = int(input('Digite um número? '))
+print(type(valor_02))
+
+# Tipo Primitivo Float (float)
+valor_03 = float(input('Digite um número? '))
+print(type(valor_03))
+
+# Tipo Primitivo Bool (bool)
+valor_04 = bool(input('Digite um valor lógico? '))
+print(type(valor_04))
