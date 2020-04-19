de97fde7830d37cd269ad4d62c861392acdd47ba
Leonardo Oliveira <leonardoc.developer@gmail.com>
2020-04-19 00:12:54 -0400

    Verificando o tipo primitivo, com type()

---
+++ Fase06/03_Funcao_type.py
@@ -0,0 +1,12 @@
+# Fase 06 - Tipos Primitivos e Saída de Dados
+# Teoria
+# Com base na aula Fase06, vamos fazer um script
+# que peça um determinado valor a ser guardado na memória
+# logo, identificando o seu tipo primitivo.
+
+n1 = input('Digite um valor: ')
+print(type(n1))
+
+# Vamos converter o seu tipo primitivo correto.
+n1 = int(input('Digite um valor: '))
+print(type(n1))
