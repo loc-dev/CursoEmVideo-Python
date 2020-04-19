5732ecb3ce3ed6d43d5f5a00d5f2c6dca13f3735
Leonardo Oliveira <leonardoc.developer@gmail.com>
2020-04-19 00:39:24 -0400

    Desafio 04 - Mostrar a soma entre dois valores digitados com .format()

---
+++ Fase06/Desafios/Desafio04.py
@@ -0,0 +1,11 @@
+#   Fase 06 - Tipos Primitivos e Saída de Dados
+#   Desafio 04
+#   Crie um programa que leia dois números e mostre a soma entre eles.
+
+n01 = float(input('Digite o primeiro valor: '))
+n02 = float(input('Digite o segundo valor: '))
+
+r = n01 + n02
+
+print('{} + {} = {}'.format(n01, n02, r))
+
