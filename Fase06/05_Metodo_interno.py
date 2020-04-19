d8e4ca62dc3b4ac47952ffc25bdf398aa46fb660
Leonardo Oliveira <leonardoc.developer@gmail.com>
2020-04-19 00:32:01 -0400

    Observando os métodos internos .isnumeric(); .isalpha() e entre outros

---
+++ Fase06/05_Metodo_interno.py
@@ -0,0 +1,9 @@
+# Fase 06 - Tipos Primitivos e Saída de Dados
+# Teoria
+# Próximo passo, é usar método interno para manipulação de uma string.
+
+a = input('Digite algo: ')
+print(a.isnumeric())
+
+b = input('Digite algo: ')
+print(b.isalpha())
