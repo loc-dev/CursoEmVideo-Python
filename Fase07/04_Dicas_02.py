ce9d5a9dcd62006e71736aa9a052b41374dc9309
Leonardo Oliveira <leonardoc.developer@gmail.com>
2020-04-22 17:27:30 -0400

    Dica 01 - Explorando os Operadores Aritméticos, fazendo o uso da concatenação e também multiplicar uma string

---
+++ Fase07/04_Dicas_02.py
@@ -0,0 +1,18 @@
+# Fase 07 - Operadores Aritméticos
+# Teoria
+# Um outra dica, é que alguns dos Operadores Aritméticos,
+# pode ser manipulados com tipo primitivo String (str).
+
+print('Exemplo 01')
+print('Oi + Olá')
+print('Oi' + 'Olá')
+print('')
+
+print('Exemplo 02')
+print('Oi *5')
+print('Oi'*5)
+print('')
+
+print('Exemplo 03')
+print('= *20')
+print('='*20)
