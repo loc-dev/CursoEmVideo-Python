2c2dc7abf182f924f8fb9cf9aefcacb8687bd747
Leonardo Oliveira <leonardoc.developer@gmail.com>
2020-04-22 16:26:55 -0400

    Observando a Ordem de Precedência com os Operadores Aritméticos

---
+++ Fase07/02_Ordem_Precedencia.py
@@ -0,0 +1,41 @@
+# Fase 07 - Operadores Aritméticos
+# Teoria
+# Precisamos entender quais são os operadores a ser executados em primeiro,
+# ou seja, a sua Ordem de Precedência.
+
+print('Exemplo 01')
+print('Resolva a expressão matemática:')
+print('5 + 3 * 2 =')
+print('')
+
+# Em Python
+print('Expressão Matemática em Python')
+print('3 * 2 = 6')
+print('5 + 6 = 11')
+print('Resultado é:', 5 + (3 * 2))
+
+print('')
+print('Exemplo 02')
+print('Resolva a expressão matemática:')
+print('3 * 5 + 4² =')
+print('')
+
+# Em Python
+print('Expressão Matemática em Python')
+print('4² = 16')
+print('3 * 5 = 15')
+print('15 + 16 = 31')
+print('Resultado é:', (3 * 5) + (4 ** 2))
+
+print('')
+print('Exemplo 03')
+print('Resolva a expressão matemática:')
+print('3 * ( 5 + 4)² =')
+print('')
+
+# Em Python
+print('Expressão Matemática em Python')
+print('( 5 + 4) = 9')
+print('( 9 )² = 81')
+print('3 * 81 = 243')
+print('Resultado é:', 3 * ((5 + 4)**2))
