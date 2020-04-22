8388d245968acea3dd8b6d2ad9108a222d04b875
Leonardo Oliveira <leonardoc.developer@gmail.com>
2020-04-22 11:38:57 -0400

    Apresentando os Operadores Aritméticos no Python

---
+++ Fase07/01_Operadores_Aritmeticos.py
@@ -0,0 +1,46 @@
+# Fase 07 - Operadores Aritméticos
+# Teoria
+# Além do operador aritmético de adição visto na aula Fase06,
+# temos outros operadores aritméticos.
+
+# Adição
+print('Adição ( + )')
+a = 5 + 2
+print('5 + 2 =', a)
+print('')
+
+# Subtração
+print('Subtração ( - )')
+s = 5 - 2
+print('5 - 2 =', s)
+print('')
+
+# Multiplicação
+print('Multiplicação ( * )')
+m = 5 * 2
+print('5 * 2 =', m)
+print('')
+
+# Divisão
+print('Divisão ( / )')
+d = 5 / 2
+print('5 / 2 =', d)
+print('')
+
+# Potência ou Exponenciação
+print('Exponenciação ( ** )')
+e = 5**2
+print('5**2 =', e)
+print('')
+
+# Divisão Inteira
+print('Divisão Inteira ( // )')
+di = 5 // 2
+print('5 // 2 =', di)
+print('')
+
+# Resto da divisão ou Módulo
+print('Módulo ( % )')
+mod = 5 % 2
+print('5 % 2 =', mod)
+print('')
