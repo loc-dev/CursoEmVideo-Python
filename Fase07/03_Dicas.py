0861512290b30ee45a14cb7167cc7ee2edcaf397
Leonardo Oliveira <leonardoc.developer@gmail.com>
2020-04-22 16:58:11 -0400

    Dica 00 - Utilização das funções internas, conhecendo o pow()

---
+++ Fase07/03_Dicas.py
@@ -0,0 +1,19 @@
+# Fase 07 - Operadores Aritméticos
+# Teoria
+# O interessante do Python são as funções internas,
+# vejamos nossa primeira dica.
+
+# Potência ou Exponenciação
+# Existe dois modos a ser utilizado em uma expressão matemática.
+
+print('Potência ou Exponenciação')
+print('1ª Modo:')
+print('A utilização do símbolo ( * ) duas vezes.')
+print('')
+print('4**3 = 64')
+
+print('')
+print('2ª Modo:')
+print('A utilização da função interna do Python, o pow().')
+print('')
+print('pow(4,3) = 64')
