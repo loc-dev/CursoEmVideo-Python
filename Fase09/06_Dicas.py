87293f8db6a3dcfe362d41d50db236188fb485da
Leonardo Oliveira <leonardoc.developer@gmail.com>
2020-07-28 21:58:45 -0400

    Aprendendo a usar a funçao print() em textos longos

---
+++ Fase09/06_Dicas.py
@@ -0,0 +1,21 @@
+# Fase 09 - Manipulando Texto
+# Teoria
+# Textos longos
+
+print("Utilizando aspas duplas três vezes no início e fim, para apresentar um texto longo")
+print("")
+print("""Quisque orci arcu, pulvinar ornare cursus sit amet, elementum et lacus. Sed nisl ex,
+efficitur ac massa id, accumsan ultrices orci. Integer congue fringilla eros. Quisque
+maximus magna odio, scelerisque lacinia mi eleifend nec. Nullam gravida mattis vulputate.
+Maecenas eget justo neque. Morbi eleifend diam facilisis velit commodo imperdiet. Nulla ac
+mauris at turpis eleifend ultricies vel sed ex. Phasellus bibendum dui non eleifend
+interdum. Maecenas suscipit arcu vel leo condimentum, sed tristique lorem volutpat. Nunc
+tincidunt arcu ex, sit amet venenatis nisl imperdiet et. Vivamus et augue id dui tempus
+convallis vitae a ante. Proin non tempor justo. Pellentesque lobortis vitae odio sit amet
+iaculis.""")
+
+
+
+
+
+
