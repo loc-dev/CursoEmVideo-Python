7f51042711f5a35e917fdfd3253c74ff804bf9f0
Leonardo Oliveira <leonardoc.developer@gmail.com>
2020-07-28 21:51:36 -0400

    Em pratica com as tecnicas

---
+++ Fase09/05_Tecnicas.py
@@ -0,0 +1,22 @@
+# Fase 09 - Manipulando Texto
+# Teoria
+# Colocando em prática as técnicas
+
+# Parte 01
+
+# Técnica de Fatiamento
+
+# Utilizando a variável 'frase'
+frase = 'Curso em Vídeo Python'
+print('Técnica de Fatiamento')
+print('Está será a frase de exemplo: ', frase)
+print('')
+
+print(frase[3])
+print(frase[3:13])
+print(frase[:13])
+print(frase[13:])
+print(frase[1:15])
+print(frase[1:15:2])
+print(frase[1::2])
+print(frase[::2])
