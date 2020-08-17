b75da9c368c9f09393cccee3fb502d08d1618131
Leonardo Oliveira <leonardoc.developer@gmail.com>
2020-08-17 14:26:34 -0400

    Maneira de simplificar uma condicao em uma linha

---
+++ Fase10/02_CondicaoSimplificada.py
@@ -0,0 +1,11 @@
+# Fase 10 - Condições ( Parte 1 )
+# Teoria
+# Estrutura Condicional
+
+# Condição Simplificada
+
+print('Estrutura condicional simplificada')
+print('Exemplo:')
+tempo = int(input('Quantos anos tem seu carro? '))
+print('Carro novo' if tempo <=3 else 'Carro velho')
+print('--FIM--')
