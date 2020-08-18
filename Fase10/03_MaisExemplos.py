a2905616e1b8985d4392d8367a4ce1797b288885
Leonardo Oliveira <leonardoc.developer@gmail.com>
2020-08-18 07:51:36 -0400

    Em pratica com os exemplos de Condicoes Simples e Composta

---
+++ Fase10/03_MaisExemplos.py
@@ -0,0 +1,25 @@
+# Fase 10 - Condições ( Parte 1 )
+# Teoria
+# Estrutura Condicional
+
+# Mais um exemplo de Condição Simples
+
+print('Estrutura Condicional')
+print('Condição Simples')
+print('Exemplo:')
+nome = str(input('Qual é o seu nome? '))
+if nome == 'Leonardo':
+    print('Que nome lindo você tem!')
+print('Bom dia {}'.format(nome))
+
+# Mais um exemplo de Condição Composta
+print('')
+print('Estrutura Condicional')
+print('Condição Composta')
+print('Exemplo:')
+nome = str(input('Qual é o seu nome? '))
+if nome == 'Leonardo':
+    print('Que nome lindo você tem!')
+else:
+    print('Seu nome é tão normal!')
+print('Bom dia, {}!'.format(nome))
