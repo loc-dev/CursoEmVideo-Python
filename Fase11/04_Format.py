c87427543daf0bffd7697d4f2ebb4dd6cfb6aad2
Leonardo Oliveira <leonardoc.developer@gmail.com>
2020-08-25 13:57:39 -0400

    Verificando outras possibilidades com o método .format() e também uma prévia de dicionário do Python

---
+++ Fase11/04_Format.py
@@ -0,0 +1,11 @@
+# Fase 11 - Cores no terminal
+# Prática
+
+# Usando o método .format() com técnica
+
+nome = 'Leonardo'
+cores = {'limpa':'\033[m',
+         'azul':'\033[34m',
+         'amarelo':'\033[33m',
+         'pretoebranco':'\033[7;30m'}
+print('Olá! Muito prazer em te conhecer, {}{}{}!!!'.format(cores['pretoebranco'], nome, cores['limpa']))
